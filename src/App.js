5569de212727b7b9af7729a8a390b2a98b13ae71
Sarthak Pawar <sarthakrajesh777@gmail.com>
2021-09-09 17:06:16 +0530

    Added css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import "./App.css";
 
 const App = () => {
+    const [result, setresult] = useState("");
+
+    const handleClick = (e) => {
+        setresult(result.concat(e.target.name))
+    }
+    const clear = () => {
+        setresult("")
+    }
+    const backspace = () => {
+        setresult(result.slice(0, result.length - 1))
+    }
+
     return (
         <>
-            <h1>Hello World</h1>
+            <div className="container">
+                <section className="screen">
+                    <form action="">
+                        <input type="text" name="" id="" value={result} />
+                    </form>
+                </section>
+                <section className="keyboard">
+                    <button className="clear special" onClick={clear}>Clear</button>
+                    <button className="backspace special" onClick={backspace}>C</button>
+                    <button className="special" name="/" onClick={handleClick}>&divide;</button>
+                    <button name="7" onClick={handleClick}>7</button>
+                    <button name="8" onClick={handleClick}>8</button>
+                    <button name="9" onClick={handleClick}>9</button>
+                    <button className="special" name="x" onClick={handleClick}>&times;</button>
+                    <button name="4" onClick={handleClick}>4</button>
+                    <button name="5" onClick={handleClick}>5</button>
+                    <button name="6" onClick={handleClick}>6</button>
+                    <button className="special" name="-" onClick={handleClick}>&ndash;</button>
+                    <button name="1" onClick={handleClick}>1</button>
+                    <button name="2" onClick={handleClick}>2</button>
+                    <button name="3" onClick={handleClick}>3</button>
+                    <button className="special" name="+" onClick={handleClick}>+</button>
+                    <button name="0" onClick={handleClick}>0</button>
+                    <button name="." onClick={handleClick}>.</button>
+                    <button className="equal special" onclick={handleClick}>=</button>
+                </section>
+            </div>
+
         </>
     )
 }
